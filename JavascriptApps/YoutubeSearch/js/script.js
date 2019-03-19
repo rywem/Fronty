8241671cdc77f4b66e83ff33b22ba3866fe05146
Ryan <cybervoid41@gmail.com>
2019-03-19 12:41:10 -0700

    Added accordian faq slider project. initial commit

--- JavascriptApps/YoutubeSearch/js/script.js
+++ JavascriptApps/YoutubeSearch/js/script.js
@@ -183,17 +183,16 @@ function getOutput(item) {
 
 function getButtons(prevPageToken, nextPageToken) {
     var btnoutput;
-    if(!prevPageToken) {
-        btnoutput = '<div class="button-container">' +
-            '<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
-            'onclick="nextPage();" >Next Page</button></div>';
-    } else {
-        btnoutput = '<div class="button-container">' +
-            '<button id="prev-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'"' +
-            'onclick="prevPage();" >Previous Page</button>'
-            '<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
-            'onclick="nextPage();" >Next Page</button></div>';
-    }
+    if(!prevPageToken){
+		var btnoutput = '<div class="button-container">'+'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
+		'onclick="nextPage();">Next Page</button></div>';
+	} else {
+		var btnoutput = '<div class="button-container">'+
+		'<button id="prev-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'"' +
+		'onclick="prevPage();">Prev Page</button>' +
+		'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
+		'onclick="nextPage();">Next Page</button></div>';
+	}
     return btnoutput;
 }
 
