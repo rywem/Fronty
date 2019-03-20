a586b4d908a5cf9d86a9ad8d2af9c3990df8b47f
Ryan <cybervoid41@gmail.com>
2019-03-19 18:28:51 -0700

    created apple-like thumb slider

---
+++ JavascriptApps/AppleSlider/js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    var totalWidth = 0;
+    var positions = new Array();
+    
+    $('#slides .slide').each(function(i) {
+        //Get slider widths
+        positions[i] = totalWidth;
+        totalWidth += $(this).width();        
+        //Check widths
+        if(!$(this).width()){
+            alert('please add a width to your images');
+            return false;
+        }
+    });
+    //Set width
+    $('#slides').width(totalWidth);
+    
+    $('#menu ul li a').click(function(e, keepScroll){
+        // Remove active class and add inactive
+        $('li.product').removeClass('active').addClass('inactive');
+        //Add active class to parent.
+        $(this).parent().addClass('active');
+        var pos = $(this).parent().prevAll('.product').length;
+        $('#slides').stop().animate({marginLeft: -positions[pos]+'px'}, 450);
+        //Prevent default
+        e.preventDefault();
+        
+        //stop auto scroll
+        if(!autoScroll){
+            clearInterval(itvl);
+        }
+    });
+    
+    //Make first image active
+    $('#menu ul li.product:first').addClass('active').siblings().addClass('inactive');
+    
+    //auto scroll
+    var current = 1;
+    function autoScroll(){
+        if(current == -1)
+            return false;
+        
+        $('#menu ul li a').eq(current%$('#menu ul li a').length).trigger('click', [true]);
+        current++;
+    }
+    
+    //Duration for auto scroll
+    var duration = 10;
+    var itvl = setInterval(function() {
+       autoScroll()
+    }, duration * 1000);
+});
